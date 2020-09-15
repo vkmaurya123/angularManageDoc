050d87a462e877742c984af4b2f4b6afc2752467
vineet <vineet@codestore.com>
2020-09-15 17:59:55 +0530

    Manage Doc commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
+import { ApiUrls } from './apiUrl';
+import { manageDocModuleApis,   } from './apiUrlsInterface';
 
 @Component({
-  selector: 'my-app',
+  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  name = 'Angular';
+export class AppComponent {
+  title = 'testApp';
+
+  public apiUrlForManageDoc : manageDocModuleApis = {
+    documentsForGridView : ApiUrls.getDocumentsForGridView,
+    createDocument : ApiUrls.createDocument,
+    updateDocument : ApiUrls.updateDocumentContent,
+    deleteDocument : ApiUrls.deleteDocument,
+    parentDocumentForSequenceUpdate : ApiUrls.getStaticParentDocumentForSequenceUpdate,
+    childDocumentForUpdateSequence : ApiUrls.getChildDocumentForUpdateSequence,
+    childDocumentSequence : ApiUrls.updateChildDocumentSequence,
+    parentDocumentSequence : ApiUrls.updateParentDocumentSequence,
+    parentDocumentWithAllChildViewOnEnumList : ApiUrls.getAllParentDocumentWithAllChildOnViewOnEnumList
+   }
+
+
+  public styleConfig = { }
 }
